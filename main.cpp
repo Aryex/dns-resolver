de5a967399b64e25a50f87ae87ac03043f986e5a
Aryex <quang1187580@gmail.com>
2022-07-16 13:48:25 -0700

    added some test code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
 
 using namespace std;
 
-int main()
+void resolveHostName(const char *hostname)
 {
-   vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-   
-   for (const string& word : msg)
+   const hostent *result = gethostbyname(hostname);
+   cout << "hostname: " << result->h_name << endl;
+
+   cout << "adresses: " << endl;
+   struct in_addr **addr_list = (struct in_addr **)result->h_addr_list;
+   for (int i = 0; addr_list[i] != NULL; i++)
    {
-      cout << word << " ";
+      cout << " - " << inet_ntoa(*addr_list[i]) << endl;
    }
-   cout << endl;
+   cout << "--------------------" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+   if (argc < 2)
+   {
+      cout << "Please enter at least one hostname" << endl;
+      return 0;
+   }
+
+   for (int i = 1; i < argc; i++)
+   {
+      resolveHostName(argv[i]);
+   }
 }
