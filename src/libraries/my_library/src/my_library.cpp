0388b55e699bac2b6140d986cd76178341e36924
Long Tran <longtr213@gmail.com>
2022-08-29 14:03:53 -0700

    add a demo library

---
+++ src/libraries/my_library/src/my_library.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "my_library.h";
+
+void initialize() {
+    std::cout << "my_library is initiated\n";
+}
+
+void terminate() {
+    std::cout << "my_library is terminated\n";
+}
