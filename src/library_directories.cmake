cfc417c8a54bfe9818c9e438713fabbbcfd30a49
Long Tran <longtr213@gmail.com>
2022-08-29 14:20:02 -0700

    set up tester 2

--- src/library_directories.cmake
+++ src/library_directories.cmake
@@ -1,3 +1,4 @@
 set(myLibraryDir "${CMAKE_SOURCE_DIR}/libraries/my_library")
 set(myLibraryIncDir "${myLibraryDir}/include")
 set(myLibraryLinkDir "${myLibraryDir}/src")
+set(myLibraryTarget "mylibrary")
---
+++ src/tests/demo_test_suite_1/tester_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "my_library.h"
+
+int main(int argc, char* argv[]) {
+    initialize();
+    terminate();
+    exit( EXIT_SUCCESS );
+}
